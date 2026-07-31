0b1176c539ff5bc682fbca29cd9559508bd354e8
Cayce Harwood <mrcaysizzle@gmail.com>
2026-07-31 15:22:01 -0400

    implementation of a reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
 import * as React from 'react';
 
+const initialStories= [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3, 
+    points: 4, 
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    }
+];
+
+const getAsyncStories = () =>
+  new Promise((resolve) => 
+    setTimeout(
+      () => resolve({ data: {stories: initialStories }}),
+      2000
+    )
+);
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
+  }
+};
+
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -12,36 +52,33 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
-const initialStories= [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3, 
-      points: 4, 
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-      }
-  ];
-
 const App = () => {
-
-  const [stories, setStories] = React.useState(initialStories);
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    getAsyncStories().then(result => {
+      dispatchStories({
+        type: 'SET_STORIES',
+        payload: result.data.stories
+      });
+      setIsLoading(false);
+    })
+    .catch(() => setIsError(true));
+  }, []);
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({
+      type: 'SET_STORIES',
+      payload: item,
+    });
   }
 
   const handleSearch = (event) => {
@@ -68,7 +105,13 @@ const App = () => {
 
     <hr />
 
-    <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    {isError && <p>Something went wrong ...</p>}
+
+    {isLoading ? (
+      <p>Loading ...</p>
+    ) : (
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+    )}
   </div>
   )
 };
@@ -85,23 +128,22 @@ const List = ({ list, onRemoveItem }) => (
   </ul>
 );
 
-const Item = ({ item, onRemoveItem }) => {
-  return (
-    <li>
-      <span>
-        <a href={item.url}>{item.title}</a> 
-      </span>
-      <span>{item.author} </span>
-      <span>{item.num_comments} </span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
-          Dismiss
-        </button>
-      </span>
-    </li>
-  )
-};
+const Item = ({ item, onRemoveItem }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a> 
+    </span>
+    <span>{item.author} </span>
+    <span>{item.num_comments} </span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </li>
+)
+
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
   const inputRef = React.useRef();
